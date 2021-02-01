f2d5cd9c040db181c63f832bafc310e4d66f5f48
wulanxiliaoliao <2055929044@qq.com>
2021-02-01 10:39:03 +0800

    Money.vue拆分为Notes，numberPad，Tags，Types四个组件

---
+++ src/components/Money/Types.vue
@@ -0,0 +1,66 @@
+<template>
+  <div>
+    <ul class="types">
+      <li class="selected">支出</li>
+      <li>收入</li>
+<!--      <li :class="type==='-'&&'selected'" @click="selectType('-')">支出</li>-->
+<!--      <li :class="type==='+'&&'selected'" @click="selectType('+')">收入</li>-->
+    </ul>
+  </div>
+</template>
+
+<script lang="ts">
+  export default {
+    name:'Types'
+  }
+  // import Vue from 'vue';
+  // import {Component} from 'vue-property-decorator';
+  //
+  // @Component({
+  //   props:{
+  //     propsMessage: String
+  //   }
+  // })
+  // export default class Type extends Vue {
+  //   type = '-'; //type为‘+’表示收入，为'-'表示支出
+  //
+  //   selectType(type: string) {
+  //     if (type !== '-' && type !== '+') {
+  //       throw new Error('type is unknown');
+  //     }
+  //     this.type = type;
+  //   }
+  //   create(){
+  //     console.log(this.propsMessage);
+  //   }
+  // };
+</script>
+
+<style lang="scss" scoped>
+  @import "~@/assets/style/helper.scss";
+
+  .types {
+    background: #c4c4c4;
+    display: flex;
+    font-size: 24px;
+
+    > li {
+      width: 50%;
+      height: 64px;
+      display: flex;
+      justify-content: center;
+      align-items: center;
+      position: relative;
+
+      &.selected::after {
+        content: '';
+        position: absolute;
+        bottom: 0;
+        left: 0;
+        width: 100%;
+        height: 4px;
+        background: #333;
+      }
+    }
+  }
+</style>
