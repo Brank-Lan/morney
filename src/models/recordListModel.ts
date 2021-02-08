843bc87e54fec81960f5a0e0e55e9159e0901fe6
wulanxiliaoliao <2055929044@qq.com>
2021-02-08 11:46:45 +0800

    抽取clone，重新封装recordListModel

--- src/models/recordListModel.ts
+++ src/models/recordListModel.ts
@@ -1,13 +1,24 @@
+import clone from '@/lib/clone';
+import createId from '@/lib/createId';
+
 const localStorageKeyName = 'recordList';
-const recordListModel = {
-  fetch: function () {
-    return JSON.parse(window.localStorage.getItem(localStorageKeyName) || '[]') as RecordItem[];
+type RecordListModel = {
+  data: RecordItem[];
+  fetch: () => RecordItem[];
+  clone(record: RecordItem): RecordItem;
+  save(recordList: RecordItem[]): void;
+};
+const recordListModel: RecordListModel = {
+  data: [],
+  fetch() {
+    this.data = JSON.parse(window.localStorage.getItem(localStorageKeyName) || '[]') as RecordItem[];
+    return this.data;
   },
-  clone(record: RecordItem) {
-    return JSON.parse(JSON.stringify(record));
+  clone(record: RecordItem[] | RecordItem) {
+    return clone(record);
   },
-  save(recordList: RecordItem[]) {
-    window.localStorage.setItem(localStorageKeyName, JSON.stringify(recordList));
+  save() {
+    window.localStorage.setItem(localStorageKeyName, JSON.stringify(this.data));
   }
 };
 export default recordListModel;
