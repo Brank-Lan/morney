f0fab3777e39d70c128cfcf3032ad3c15f8cc6b8
wulanxiliaoliao <2055929044@qq.com>
2021-02-16 20:05:53 +0800

    Statistics.vue使用Tabs组件

--- src/views/Statistics.vue
+++ src/views/Statistics.vue
@@ -1,37 +1,51 @@
 <template>
   <div>
     <Layout>
-      <p>{{count}}</p>
-      <button @click="$store.commit('increment',10)">count+1</button>
+      <Tabs :value.sync="type" class-prefix="type" :data-source="typeList"/>
+      <Tabs :value.sync="interval" class-prefix="interval" :data-source="intervalList"/>
     </Layout>
   </div>
 </template>
 
 <script lang="ts">
   import Vue from 'vue';
-  import store from '@/store/index2';
   import {Component} from 'vue-property-decorator';
   import Button from '@/components/Button.vue';
+  import Types from '@/components/Money/Types.vue';
+  import Tabs from '@/components/Tabs.vue';
+
 
   @Component({
-    components: {Button},
-    computed: {
-      count() {
-        return this.$store.state.count;
-      }
-    }
+    components: {Tabs, Types, Button},
   })
   export default class Statistics extends Vue {
-    addCount() {
-      store.add();
-    }
+    interval = 'day';
+    type = '-';
+    typeList = [{text: '支出', value: '-'}, {text: '收入', value: '+'}];
+    intervalList = [
+      {text: '按天', value: 'day'},
+      {text: '按周', value: 'week'},
+      {text: '按月', value: 'Month'},
+    ];
   }
 </script>
 
 <style lang="scss" scoped>
   @import "~@/assets/style/helper.scss";
 
-  p {
-    color: $color-highlight;
+  ::v-deep .type-tabs-item {
+    background: #c4c4c4;
+
+    &.selected {
+      background: white;
+    }
+
+    &::after {
+      display: none;
+    }
+  }
+
+  ::v-deep .interval-tabs-item {
+    height: 48px;
   }
 </style>
