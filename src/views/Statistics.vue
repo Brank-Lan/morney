54f89950ee9c5fe28e98d77d9266867656f311cc
wulanxiliaoliao <2055929044@qq.com>
2021-02-18 12:21:52 +0800

    数据排序分组

--- src/views/Statistics.vue
+++ src/views/Statistics.vue
@@ -4,8 +4,8 @@
       <Tabs :value.sync="type" class-prefix="type" :data-source="typeList"/>
       <Tabs :value.sync="interval" class-prefix="interval" :data-source="intervalList"/>
       <ol>
-        <li v-for="(group,index) in result" :key="index">
-          <h3 class="title">{{group.title}}</h3>
+        <li v-for="(group,index) in groupList" :key="index">
+          <h3 class="title">{{beauty(group.title)}}</h3>
           <ol>
             <li class="record" v-for="(item, index) in group.items" :key="index">
               <span>{{tagString(item.tags)}}</span>
@@ -27,6 +27,8 @@
   import Tabs from '@/components/Tabs.vue';
   import intervalList from '@/constant/intervalList';
   import typeList from '@/constant/typeList';
+  import clone from '@/lib/clone';
+  import dayjs from 'dayjs';
 
 
   @Component({
@@ -37,16 +39,23 @@
       return (this.$store.state as RootState).recordList;
     }
 
-    get result() {
+    get groupList() {
       const {recordList} = this;
-      type HashTableValue = { title: string; items: RecordItem[] }
-      const hashTable: { [key: string]: HashTableValue } = {};
-      for (let i = 0; i < recordList.length; i++) {
-        const [date, time] = recordList[i].createAt!.split('T');
-        hashTable[date] = hashTable[date] || {title: date, items: []};
-        hashTable[date].items.push(recordList[i]);
+      /*[
+        {title,item[]}
+      ]*/
+      const newList = clone(recordList).sort((a: RecordItem, b: RecordItem) => {return dayjs(b.createAt).valueOf() - dayjs(a.createAt).valueOf();});
+      const result = [{title: newList[0].createAt, items: [newList[0]]}];
+      for (let i = 0; i < newList.length; i++) {
+        const current = newList[i];
+        const last = result[result.length - 1];
+        if (dayjs(last.title).isSame(dayjs(current.createAt), 'day')) {
+          last.items.push(current);
+        } else {
+          result.push({title: current.createAt, items: [current]});
+        }
       }
-      return hashTable;
+      return result;
     }
 
     interval = 'day';
@@ -56,6 +65,22 @@
 
     beforeCreate() {
       this.$store.commit('fetchRecordList');
+    }
+
+    beauty(string: string) {
+      const now = dayjs();
+      const day = dayjs(string);
+      if (day.isSame(now, 'day')) {
+        return '今天';
+      } else if (day.isSame(now.subtract(1, 'day'), 'day')) {
+        return '昨天';
+      } else if (day.isSame(now.subtract(2, 'day'), 'day')) {
+        return '前天';
+      } else if (day.isSame(now, 'year')) {
+        return day.format('M月D日');
+      } else {
+        return day.format('YYYY年M月D日');
+      }
     }
 
     tagString(tags: Tag[]) {
